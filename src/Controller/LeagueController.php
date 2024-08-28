f5ff92703127c1e6c25cd64e5057a5d6bd6c26e5
Nicolas Tran <contact.nicolastran@gmail.com>
2024-08-28 10:59:08 +0200

    update schedule function

--- src/Controller/LeagueController.php
+++ src/Controller/LeagueController.php
@@ -19,6 +19,12 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 class LeagueController extends AbstractController
 {
     private string $apiUrl;
+    private $selectedLeagues = [
+        'LEC' => '98767991302996019',
+        'LCS' => '98767991299243165',
+        'LCK' => '98767991310872058',
+        'LPL' => '98767991314006698',
+    ];
 
     public function __construct(
         private ParameterBagInterface $parameterBag,
@@ -70,6 +76,10 @@ class LeagueController extends AbstractController
         $onlyNextMatches = $request->get("onlyNextMatches", 0);
 
         // TO DO Move all results and map in entity so $scheduleDecoded->data->schedule->events is defined -> interface|entity
+
+        if ($id === 'selected') {
+            $id = implode(',', $this->selectedLeagues);
+        }
 
         $schedule = $this->fetch('GET', '/getSchedule', ['query' => [
             'hl' => 'fr-FR',
